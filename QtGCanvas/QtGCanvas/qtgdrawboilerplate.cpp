1776801e233313bd7ce6f1bceb1e24d6777c8ddb
Daniel Fairchild <daniel.fairchild@gmail.com>
2014-06-25 14:29:36 +0200

    parameterized drawable objects

---
+++ QtGCanvas/QtGCanvas/qtgdrawboilerplate.cpp
@@ -0,0 +1,5 @@
+#include "qtgdrawboilerplate.h"
+
+QtGDrawBoilerPlate::QtGDrawBoilerPlate()
+{
+}
---
+++ QtGCanvas/qtgdrawboilerplate.h
@@ -0,0 +1,10 @@
+#ifndef QTGDRAWBOILERPLATE_H
+#define QTGDRAWBOILERPLATE_H
+
+class QtGDrawBoilerPlate
+{
+public:
+    QtGDrawBoilerPlate();
+};
+
+#endif // QTGDRAWBOILERPLATE_H
