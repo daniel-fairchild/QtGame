efad883cd42a747a9c185193225b883e4cde0a26
Daniel Fairchild <daniel.fairchild@gmail.com>
2014-08-23 08:44:02 +0200

    push for LD30

--- QtGSound/qtgsound.cpp
+++ QtGSound/qtgsound.cpp
@@ -1,6 +1,32 @@
 #include "qtgsound.h"
+#include "qdebug.h"
 
-
-QtGSound::QtGSound()
+QtGSound::QtGSound(QObject* parent)
 {
+    _parent = parent;
+}
+
+int QtGSound::load(const char *fname)
+{
+    QSoundEffect* tmp = new QSoundEffect(_parent);
+
+    QUrl qrl = QUrl::fromLocalFile(fname);
+    tmp->setSource(qrl);
+    tmp->setLoopCount(1);
+    _sfx.append(tmp);
+
+    tmp->setVolume(1.0f);
+    tmp->setLoopCount(1);
+    tmp->stop();
+    //    while (tmp.status() != QSoundEffect::Ready)
+    //        qDebug() << tmp->status();
+
+    return _sfx.length()-1;
+}
+
+void QtGSound::play(int ref, float volume, float pos)
+{
+    QSoundEffect* tmp = _sfx[ref];
+    tmp->setVolume(volume);
+    tmp->play();
 }
--- QtGSound/qtgsound.h
+++ QtGSound/qtgsound.h
@@ -1,12 +1,25 @@
 #ifndef QTGSOUND_H
 #define QTGSOUND_H
 
+#include <qlist.h>
+#include <QSoundEffect>
+#include <QObject>
 
 class QtGSound
 {
 
 public:
-    QtGSound();
+    QtGSound(QObject* parent = 0);
+
+    int load(const char* fname);
+
+    void play(int ref, float volume, float pos);
+
+
+
+private:
+    QList<QSoundEffect*> _sfx;
+    QObject* _parent;
 };
 
 #endif // QTGSOUND_H
